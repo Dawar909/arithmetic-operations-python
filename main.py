6f5e55fdca716baa5667b3fae9cb70115df56f5c
Dawar Ashai <ashai.dawar0@gmail.com>
2024-11-22 15:30:07 +0530

    added program

---
+++ main.py
@@ -0,0 +1,7 @@
+x =208937890
+y =354678032
+print("we are going to  see all the posiible operations with variables")
+print(f"x = {x}")
+print(f"y = {y}")
+print(f"the results are {x+y}, {y-x}, {x*y}, {y/x} and {y%x}")
+print(f"The sum is {x+y} the difference is {y-x} the product is {x*y} the quotient is {y/x} the remainder is {y%x}")
